ea99474ccacafccc4dda534adec5e20aa39ed71c
Ganesh-999-ops <marripalligan96@gmail.com>
2025-09-02 22:41:05 +0530

    Nodejs-Dockerfile

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Hello from Node.js running inside Docker!");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
